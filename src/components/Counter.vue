ab08c7d5f0b43ffa3a88f294caf4eeffad14cc90
kt_boom <kritsada.tan@hotmail.com>
2021-07-18 21:57:51 +0700

    add btn

---
+++ src/components/Counter.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="counter">
+        Counter : {{ number }}
+        <div>
+            <label for="counter" v-bind:title="number">Counter: </label>
+            <input type="text" v-model="number">
+            <button v-on:click="increase">+</button>
+            <button v-on:click="decrease">-</button>
+            <button v-on:click="increase10">+10</button>
+            <button v-on:click="multiply">*2</button>
+
+        </div>
+        
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            number : 10,
+        }
+    },
+    
+    methods: {
+        increase() {
+            this.number++;
+            console.log(this.number);
+        },
+        decrease() {
+            this.number--;
+        },
+        increase10() {
+            this.number+=10;
+        },
+        multiply() {
+            this.number*=2;
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>>
+
---
+++ src/views/NumberList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+  </div>
+</template>
+
+<script>
+import Counter from '@/components/Counter'
+export default {
+    components : {
+        Counter
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>>
+
